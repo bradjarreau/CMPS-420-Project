d00d39f0349068bbc2735faf2c50e34868fdb9ef
bradjarreau <125139148+bradjarreau@users.noreply.github.com>
2024-10-22 14:40:36 -0500

    Create Evacuation.js

---
+++ app/Evacuation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EvacuationInfo = () => {
+  return (
+    <View>
+      <Text>Evacuation Information</Text>
+    </View>
+  );
+};
+
+export default EvacuationInfo;
