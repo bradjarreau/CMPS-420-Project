00fd20e5a18bdc4416a7bff82227ce62e76a3215
bradjarreau <125139148+bradjarreau@users.noreply.github.com>
2024-10-22 14:41:49 -0500

    Create PreparationAndSafety.js

---
+++ app/PreparationAndSafety.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const PreparationAndSafety = () => {
+  return (
+    <View>
+      <Text>Preparation and Safety Tips</Text>
+    </View>
+  );
+};
+
+export default PreparationAndSafety;
